84d0b6d3d01e54113a5f830fd9bd27acde49e2b4
tomer_shulner <tomer.shulner@innoviz-tech.com>
2023-12-20 15:30:16 +0200

    try new way for random

--- GenThree.java
+++ GenThree.java
@@ -7,9 +7,9 @@ public class GenThree {
 		int lower_limit = Integer.parseInt(args[0]);
 		int higher_limit = Integer.parseInt(args[1]);
 		
-		int a = (int) ((Math.random() * (higher_limit - lower_limit)) + lower_limit);
-		int b = (int) ((Math.random() * (higher_limit - lower_limit)) + lower_limit);
-		int c = (int) ((Math.random() * (higher_limit - lower_limit)) + lower_limit);
+		int a = (int) ((Math.random() * (higher_limit - lower_limit + 1)) + lower_limit);
+		int b = (int) ((Math.random() * (higher_limit - lower_limit + 1)) + lower_limit);
+		int c = (int) ((Math.random() * (higher_limit - lower_limit + 1)) + lower_limit);
 		
 		int minimal = Math.min(Math.min(a, b), c);
 		
