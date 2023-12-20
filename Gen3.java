a073dd80a32e2c5a1f0af8adcedf9c5f41569380
tomer_shulner <tomer.shulner@innoviz-tech.com>
2023-12-20 15:05:00 +0200

    fix class name

--- Gen3.java
+++ Gen3.java
@@ -2,7 +2,7 @@
  * Generates three random integers, each in a given range [a,b),
  * prints them, and then prints the minimal number that was generated.
  */
-public class GenThree {
+public class Gen3 {
 	public static void main(String[] args) {
 		int lower_limit = Integer.parseInt(args[0]);
 		int higher_limit = Integer.parseInt(args[1]);
