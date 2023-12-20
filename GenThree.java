517df24855e961a643cef88e104f8a2291eb9552
tomer_shulner <tomer.shulner@innoviz-tech.com>
2023-12-20 15:43:57 +0200

    add check for variables in gen3

--- GenThree.java
+++ GenThree.java
@@ -4,12 +4,23 @@
  */
 public class GenThree {
 	public static void main(String[] args) {
-		int lower_limit = Integer.parseInt(args[0]);
-		int higher_limit = Integer.parseInt(args[1]);
+		int first_limit = Integer.parseInt(args[0]);
+		int second_limit = Integer.parseInt(args[1]);
+		int higher_limit;
+		int lower_limit;
 		
-		int a = (int) ((Math.random() * (higher_limit - lower_limit + 1)) + lower_limit);
-		int b = (int) ((Math.random() * (higher_limit - lower_limit + 1)) + lower_limit);
-		int c = (int) ((Math.random() * (higher_limit - lower_limit + 1)) + lower_limit);
+		if (first_limit < second_limit) {
+			higher_limit = second_limit;
+			lower_limit = first_limit;
+		}
+		else {
+			higher_limit = first_limit;
+			lower_limit = second_limit;
+		}
+		
+		int a = (int) ((Math.random() * (higher_limit - lower_limit)) + lower_limit);
+		int b = (int) ((Math.random() * (higher_limit - lower_limit)) + lower_limit);
+		int c = (int) ((Math.random() * (higher_limit - lower_limit)) + lower_limit);
 		
 		int minimal = Math.min(Math.min(a, b), c);
 		
