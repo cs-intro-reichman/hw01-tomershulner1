dea3aaa9932554c865b6280c4b88e258ccdef7f7
tomer_shulner <tomer.shulner@innoviz-tech.com>
2023-12-20 15:17:57 +0200

    change names

--- GenThree.java
+++ GenThree.java
@@ -2,14 +2,14 @@
  * Generates three random integers, each in a given range [a,b),
  * prints them, and then prints the minimal number that was generated.
  */
-public class Gen3 {
+public class GenThree {
 	public static void main(String[] args) {
 		int lower_limit = Integer.parseInt(args[0]);
 		int higher_limit = Integer.parseInt(args[1]);
 		
-		int a = (int) ((Math.random() * (higher_limit - lower_limit)) + lower_limit + 1);
-		int b = (int) ((Math.random() * (higher_limit - lower_limit)) + lower_limit + 1);
-		int c = (int) ((Math.random() * (higher_limit - lower_limit)) + lower_limit + 1);
+		int a = (int) ((Math.random() * (higher_limit - lower_limit)) + lower_limit);
+		int b = (int) ((Math.random() * (higher_limit - lower_limit)) + lower_limit);
+		int c = (int) ((Math.random() * (higher_limit - lower_limit)) + lower_limit);
 		
 		int minimal = Math.min(Math.min(a, b), c);
 		
