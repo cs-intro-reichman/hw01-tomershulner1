75bfbb678406600b2429cf2b1b084a11f587f4b3
tomer_shulner <tomer.shulner@innoviz-tech.com>
2023-12-20 10:16:58 +0200

    first commit\

--- AddTwo.java
+++ AddTwo.java
@@ -3,6 +3,8 @@
  */
 public class AddTwo {
 	public static void main(String[] args) {
-		// Put your code here	
+		int num1 = Integer.parseInt(args[0]), num2 = Integer.parseInt(args[1]);
+		int result = num1 + num2;
+		System.out.println(num1 + " + " + num2 + " = " + result);
 	}
 }
