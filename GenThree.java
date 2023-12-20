3760e7b031373193f602b510dea29d368953cce5
tomer_shulner <tomer.shulner@innoviz-tech.com>
2023-12-20 15:50:51 +0200

    change range way

--- GenThree.java
+++ GenThree.java
@@ -6,21 +6,21 @@ public class GenThree {
 	public static void main(String[] args) {
 		int first_limit = Integer.parseInt(args[0]);
 		int second_limit = Integer.parseInt(args[1]);
-		int higher_limit;
+		int range
 		int lower_limit;
 		
 		if (first_limit < second_limit) {
-			higher_limit = second_limit;
+			range = second_limit - first_limit;
 			lower_limit = first_limit;
 		}
 		else {
-			higher_limit = first_limit;
-			lower_limit = second_limit;
+			range = first_limit - second_limit;
+			higher_limit = second_limit;
 		}
 		
-		int a = (int) ((Math.random() * (higher_limit - lower_limit)) + lower_limit);
-		int b = (int) ((Math.random() * (higher_limit - lower_limit)) + lower_limit);
-		int c = (int) ((Math.random() * (higher_limit - lower_limit)) + lower_limit);
+		int a = (int) ((Math.random() * range) + lower_limit);
+		int b = (int) ((Math.random() * range) + lower_limit);
+		int c = (int) ((Math.random() * range) + lower_limit);
 		
 		int minimal = Math.min(Math.min(a, b), c);
 		
